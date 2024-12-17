706b46a86ce0cd3a51bd921ba0c734b8d662b39c
Bezrukov47 <bezr47@gmail.com>
2024-12-17 17:32:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# Условная конструкция. Операторы: if, elif, else
+first = 25
+second = 87
+third = 89
+print (first)
+print (second)
+print (third)
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print (2)
+else: # Если ни одно из условий не выполнено:
+    print (0)
+# Второй вариант выполнения условия: Если равных чисел среди 3-х вообще нет, то вывести 0
+if_elif_else: first == second or first == third or second == third
+print(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
